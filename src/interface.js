7631c4941bbad17e5d657ec4655f47a159aa5371
DanielaGSB <Daniela.galeote@gmail.com>
2016-08-24 21:35:31 +0100

    User interface for temperature decrease and reset

--- src/interface.js
+++ src/interface.js
@@ -3,9 +3,18 @@ $(document).ready(function() {
 
   $('#temperature').text(thermostat.temperature);
 
-
   $('#temperature-increase').on('click', function() {
     thermostat.increaseTemperature();
+    $('#temperature').text(thermostat.temperature);
+  });
+
+  $('#temperature-decrease').on('click', function() {
+    thermostat.decreaseTemperature();
+    $('#temperature').text(thermostat.temperature);
+  });
+
+  $('#temperature-reset').on('click', function() {
+    thermostat.resetTemperature();
     $('#temperature').text(thermostat.temperature);
   });
 });
