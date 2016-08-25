ede9fa1291e751d53305ba2e8f2d53745f3c6950
DanielaGSB <Daniela.galeote@gmail.com>
2016-08-25 19:07:47 +0100

    API call added to display current temp in London

--- src/interface.js
+++ src/interface.js
@@ -3,28 +3,25 @@ $(document).ready(function() {
   var color = 'orange';
 
   updateTemperature();
-  energyColour();
 
   function updateTemperature() {
     $('#temperature').text(thermostat.temperature);
+    $('#temperature').attr('class', thermostat.energyUsage());
   }
 
   $('#temperature-increase').on('click', function() {
     thermostat.increaseTemperature();
     updateTemperature();
-    energyColour();
   });
 
   $('#temperature-decrease').on('click', function() {
     thermostat.decreaseTemperature();
     updateTemperature();
-    energyColour();
   });
 
   $('#temperature-reset').on('click', function() {
     thermostat.resetTemperature();
     updateTemperature();
-    energyColour();
   });
 
   $('#powersavingmode-on').on('click', function() {
@@ -37,18 +34,7 @@ $(document).ready(function() {
     $('#power-saving-status').text('off');
   });
 
-  function energyColour() {
-    if (thermostat.energyUsage() == 'low') {
-      color = 'green';
-    } else if (thermostat.energyUsage() == 'high') {
-      color = 'red'
-    }
-    $('#temperature').css('color', color);
-  }
-// });
-  //
-  // The thermostat should colour the display based on energy usage
-  //  - < 18 is green, < 25 is yellow, otherwise red
-
-
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric&appid=45fd069205600da26b1ef07304444c8d', function(data) {
+    $('#current-temperature').text(data.main.temp);
+  });
 });
